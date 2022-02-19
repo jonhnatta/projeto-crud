00a4ff96d2300dd2e26cc52b77f604cd00e8eb55
jonhnatta <jonatta89@gmail.com>
2022-02-18 23:24:52 -0300

    adicionando estrutura inicial de pastas

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+//definindo o template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//difinindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Habilita server para receber dados via post formulário
+app.use(express.urlencoded({extended: true}))
+
+//rotas
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'CRUD'
+  })
+})
+
+// 404 error (not found)
+app.use((req, res) => {
+  res.send('Página não encontrada')
+})
+
+//executa o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Server is http://127.0.0.1:${port}/ listening on port ${port}`)
+})
